4a79bd6b3b3c0cd01e71a54865234f6d3dfecee7
xufengfeng <xu-fengfeng@msn.com>
2018-06-23 23:09:18 +0800

    增加消息对话框

--- execise2.py
+++ execise2.py
@@ -1,7 +1,11 @@
+#coding=utf-8
 import os
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.Qt import *
+
+import PyQt5
 
 #class hiren
 
@@ -13,14 +17,35 @@ class MyWidget(QWidget):
         self.setWindowTitle("Test Program")
     def addIcon(self):
         icon = QIcon("shell.ico")
-        self.setWindowIcon(icon)        
+        self.setWindowIcon(icon)  
+    
+    def about_function(self):
+        print("Message Box")
+        QMessageBox.about(self, "about the project", "about the project")
+        QMessageBox.aboutQt(self)
+        
+    def addButton(self):
+        about_button = QPushButton("关于", self)  #创造一个pushbutton，父窗口是这个类实例
+        about_button.setGeometry(0, 0, 70 ,30)
+        about_button.clicked.connect(self.about_function)
+        
+        textline = QLineEdit(self)
+        textline.setGeometry(0, 30, 70, 30)
+        
+        
+        
+                  
     def Setup(self):
+        self.setGeometry(300, 300, 300, 100)
         self.addTitle()
         self.addIcon()
+        self.addButton()
         self.show()
         
 if __name__ == "__main__":
     print("program start..")
+    print(PyQt5.__name__)
+    
     app = QApplication(sys.argv)
     icn = MyWidget()
     
