d6522d947849763a3eba4a5cd2aa485ad40c2bfb
xufengfeng <xufengfeng@DESKTOP-NCDH78L>
2018-06-23 21:27:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+from PyQt5.QtWidgets import QWidget, QApplication
+
+if __name__ == "__main__":
+    print("program start..")
+    app =QApplication(sys.argv)
+    w = QWidget()
+    w.setWindowTitle("Test Program")
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.show()
+    sys.exit(app.exec_())
+    
+    
+    print("porgram finished.")
